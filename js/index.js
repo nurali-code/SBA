a578558292601bdd1db391a30864519f9d623e4e
nurali-code <nur3.dav.97@gmail.com>
2023-12-27 11:44:22 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,123 @@
+$(function () {
+    $('#register-phone').inputmask('+9 (999) 999-99-99');
+    $('.modal-question__close').on('click', function () {
+        $('.modal-question').addClass('off');
+    });
+    $('.fix-question').on('click', function () {
+        $('.modal-question').removeClass('off');
+    });
+    $('#myForm').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true,
+            },
+            name: {
+                required: true,
+            },
+            message: {
+                required: true,
+            },
+        },
+        messages: {
+            email: {
+                required: 'Поле не заполнено или заполнено не верно',
+                email: 'Поле не заполнено или заполнено не верно',
+            },
+            name: {
+                required: 'Поле не заполнено или заполнено не верно',
+            },
+            message: {
+                required: 'Поле не заполнено или заполнено не верно',
+            },
+        },
+        submitHandler: function () {
+            alert('Валидация успешна!');
+        },
+    });
+    $('#registerForm').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true,
+            },
+            name: {
+                required: true,
+            },
+            phone: {
+                required: true,
+                checkMask: true,
+            },
+        },
+        messages: {
+            email: {
+                required: 'Поле не заполнено или заполнено не верно',
+                email: 'Поле не заполнено или заполнено не верно',
+            },
+            name: {
+                required: 'Поле не заполнено или заполнено не верно',
+            },
+            phone: {
+                required: 'Поле не заполнено или заполнено не верно',
+                checkMask: 'Введите полный номер телефона',
+            },
+        },
+        submitHandler: function () {
+            alert('Валидация успешна!');
+        },
+    });
+    function windowSize() {
+        if ($(window).width() >= '999') {
+            $('.tab-1').hover(function () {
+                $('.tab').addClass('tab-none');
+                $('#tab-1').removeClass('tab-none');
+                $('.tab-content').removeClass('tab-hover');
+                $(this).addClass('tab-hover');
+            });
+            $('.tab-2').hover(function () {
+                $('.tab').addClass('tab-none');
+                $('#tab-2').removeClass('tab-none');
+                $('.tab-content').removeClass('tab-hover');
+                $(this).addClass('tab-hover');
+            });
+            $('.tab-3').hover(function () {
+                $('.tab').addClass('tab-none');
+                $('#tab-3').removeClass('tab-none');
+                $('.tab-content').removeClass('tab-hover');
+                $(this).addClass('tab-hover');
+            });
+        } else {
+            $('.tab-1').on('click', function () {
+                $('.tab').addClass('tab-none');
+                $('#tab-1').removeClass('tab-none');
+                $('.tab-content').removeClass('tab-hover');
+                $(this).addClass('tab-hover');
+            });
+            $('.tab-2').on('click', function () {
+                $('.tab').addClass('tab-none');
+                $('#tab-2').removeClass('tab-none');
+                $('.tab-content').removeClass('tab-hover');
+                $(this).addClass('tab-hover');
+            });
+            $('.tab-3').on('click', function () {
+                $('.tab').addClass('tab-none');
+                $('#tab-3').removeClass('tab-none');
+                $('.tab-content').removeClass('tab-hover');
+                $(this).addClass('tab-hover');
+            });
+        }
+    }
+    $(window).on('load resize', windowSize);
+
+    $('.programs-block').on('click', function () {
+        $('.programs-block-active').slideUp();
+        $('.programs-button .active, .programs-button').removeClass('active');
+        $(this)
+            .find('.programs-row')
+            .find('.programs-button')
+            .addClass('active');
+        $(this).find('.programs-block-active').slideDown();
+    })
+
+
+});
